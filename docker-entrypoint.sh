85dbb7a672796179c64d7c8d682b4862555a7532
Nick Sorrell <nsorrell@intxlog.com>
2019-06-06 15:55:33 -0400

    Adding optional bootstrap.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ idmod www-data "$USER_UID" "$USER_GID" || true
 
 rm -f /var/run/apache2/apache2.pid
 
+if [ -s "/bootstrap/bootstrap.sh" ]; then
+    ./bootstrap/bootstrap.sh 
+fi
+
 if [ $# -ne 0 ]; then
     exec tini -- "$@"
 fi
